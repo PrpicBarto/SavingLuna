ce297493004c7cabd4211ece65f41c6e5fe0ab82
FranNovacic <fnovacic@gmail.com>
2025-07-31 13:23:55 +0200

    svjetla,grafika,lampe

---
+++ SavingLuna/Assets/_Scripts/BlueSpotLightScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpotLightScript : MonoBehaviour
+{
+    private float lightIntensityAverage = 52.89f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
