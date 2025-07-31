61cc08dc6304ccf0cbf4b29e316f27cbea8338c8
PrpicBarto <barto.prpic@gmail.com>
2025-07-31 09:51:52 +0200

    Added AI basics

--- SavingLuna/Assets/_Scripts/Zombie/ZombieEnemyMovementScript.cs
+++ SavingLuna/Assets/_Scripts/Zombie/ZombieEnemyMovementScript.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class ZombieEnemyMovementScript : MonoBehaviour
@@ -6,14 +7,48 @@ public class ZombieEnemyMovementScript : MonoBehaviour
     public Transform playerTarget;
     [SerializeField] public float movementSpeed = 5f;
     [SerializeField] private float damage = 20f;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float distanceToPlayer;
 
+    [SerializeField] private bool pathCalculate = false;
+
+    Vector3 startingPoint;
+    [SerializeField] NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        startingPoint = transform.position;
+    }
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            playerTarget.position,
-            movementSpeed * Time.deltaTime);
-        transform.LookAt(playerTarget.position);
+        distanceToPlayer = Vector3.Distance(navMeshAgent.transform.position, playerTarget.position);
+        if(distanceToPlayer < attackRange)
+        {
+            navMeshAgent.isStopped = true;
+            // Attack the player
+            Debug.Log("Zombie is attacking the player!");
+        }
+        else
+        {
+            navMeshAgent.isStopped = false;
+            if(!navMeshAgent.hasPath && pathCalculate)
+            {
+                navMeshAgent.destination = startingPoint;
+                pathCalculate = false;
+            }
+            else
+            {
+                navMeshAgent.destination = playerTarget.position;
+                pathCalculate = true;
+            }
+        }
+
+        //transform.position = Vector3.MoveTowards(
+        //    transform.position,
+        //    playerTarget.position,
+        //    movementSpeed * Time.deltaTime);
+        //transform.LookAt(playerTarget.position);
     }
 
     private void OnTriggerEnter(Collider other)
