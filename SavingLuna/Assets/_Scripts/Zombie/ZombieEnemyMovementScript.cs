1f8e9288f7f5d04392de0a9ad386a5513ad7bf3d
PrpicBarto <barto.prpic@gmail.com>
2025-07-31 13:23:08 +0200

    Added new assets and colliders

--- SavingLuna/Assets/_Scripts/Zombie/ZombieEnemyMovementScript.cs
+++ SavingLuna/Assets/_Scripts/Zombie/ZombieEnemyMovementScript.cs
@@ -1,13 +1,12 @@
-using System;
 using UnityEngine.AI;
 using UnityEngine;
 
 public class ZombieEnemyMovementScript : MonoBehaviour
 {
     public Transform playerTarget;
-    [SerializeField] public float movementSpeed = 5f;
+    [SerializeField] public float movementSpeed = 2f;
     [SerializeField] private float damage = 20f;
-    [SerializeField] private float attackRange;
+    [SerializeField] private float attackRange = 7.5f;
     [SerializeField] private float distanceToPlayer;
 
     [SerializeField] private bool pathCalculate = false;
@@ -15,40 +14,16 @@ public class ZombieEnemyMovementScript : MonoBehaviour
     Vector3 startingPoint;
     [SerializeField] NavMeshAgent navMeshAgent;
 
-    private void Start()
-    {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        startingPoint = transform.position;
-    }
+    public TriggerZoneScript myZone;
+
+
     private void Update()
     {
-        distanceToPlayer = Vector3.Distance(navMeshAgent.transform.position, playerTarget.position);
-        if(distanceToPlayer < attackRange)
-        {
-            navMeshAgent.isStopped = true;
-            // Attack the player
-            Debug.Log("Zombie is attacking the player!");
-        }
-        else
-        {
-            navMeshAgent.isStopped = false;
-            if(!navMeshAgent.hasPath && pathCalculate)
-            {
-                navMeshAgent.destination = startingPoint;
-                pathCalculate = false;
-            }
-            else
-            {
-                navMeshAgent.destination = playerTarget.position;
-                pathCalculate = true;
-            }
-        }
-
-        //transform.position = Vector3.MoveTowards(
-        //    transform.position,
-        //    playerTarget.position,
-        //    movementSpeed * Time.deltaTime);
-        //transform.LookAt(playerTarget.position);
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            playerTarget.position,
+            movementSpeed* Time.deltaTime);
+        transform.LookAt(playerTarget.position);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -56,13 +31,12 @@ public class ZombieEnemyMovementScript : MonoBehaviour
         if (other.TryGetComponent(out PlayerHealth playerHealth))
         {
             playerHealth.TakeDamage(damage);
-            Debug.Log("Zombie attacked the player!");
         }
-        if(other.TryGetComponent(out PlayerMovement playerMovement))
+        if (other.TryGetComponent(out PlayerMovement playerMovement))
         {
             Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
-                playerRigidbody.AddForce(-playerMovement.transform.forward * 50, ForceMode.Impulse);
-                Debug.Log("Zombie pushed the player!");
+            playerRigidbody.AddForce(-playerMovement.transform.forward * 50, ForceMode.Impulse);
         }
     }
+
 }
